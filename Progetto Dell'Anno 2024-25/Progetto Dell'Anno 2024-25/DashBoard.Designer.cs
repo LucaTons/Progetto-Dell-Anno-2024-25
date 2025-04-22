8e38aa6b5ebb4202f359879262673af40f9a337b
LUCA TONELLI <st11004@iismarconipieralisi.it>
2025-04-22 18:25:20 +0200

    Modifica

--- Progetto Dell'Anno 2024-25/Progetto Dell'Anno 2024-25/DashBoard.Designer.cs
+++ Progetto Dell'Anno 2024-25/Progetto Dell'Anno 2024-25/DashBoard.Designer.cs
@@ -38,10 +38,20 @@
             this.tabPageVisualizza = new System.Windows.Forms.TabPage();
             this.Visualizza_Spese = new System.Windows.Forms.ListView();
             this.tabPageReport = new System.Windows.Forms.TabPage();
+            this.comboBox_Mesi = new System.Windows.Forms.ComboBox();
             this.tabPageImpostazioni = new System.Windows.Forms.TabPage();
+            this.comboBox_CambiaTema = new System.Windows.Forms.ComboBox();
+            this.textBox_Budget = new System.Windows.Forms.TextBox();
+            this.button_ImpostaBudget = new System.Windows.Forms.Button();
+            this.label_CambiaTema = new System.Windows.Forms.Label();
+            this.label_ImpostaBudget = new System.Windows.Forms.Label();
+            this.label_Visualizza = new System.Windows.Forms.Label();
+            this.label_Aggiungi = new System.Windows.Forms.Label();
             this.tabControlOperazioni.SuspendLayout();
             this.tabPageAggiungi.SuspendLayout();
             this.tabPageVisualizza.SuspendLayout();
+            this.tabPageReport.SuspendLayout();
+            this.tabPageImpostazioni.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControlOperazioni
@@ -53,11 +63,12 @@
             this.tabControlOperazioni.Location = new System.Drawing.Point(0, 12);
             this.tabControlOperazioni.Name = "tabControlOperazioni";
             this.tabControlOperazioni.SelectedIndex = 0;
-            this.tabControlOperazioni.Size = new System.Drawing.Size(1180, 465);
+            this.tabControlOperazioni.Size = new System.Drawing.Size(1800, 900);
             this.tabControlOperazioni.TabIndex = 1;
             // 
             // tabPageAggiungi
             // 
+            this.tabPageAggiungi.Controls.Add(this.label_Aggiungi);
             this.tabPageAggiungi.Controls.Add(this.textBox_Prezzo);
             this.tabPageAggiungi.Controls.Add(this.ComboBox_Categoria);
             this.tabPageAggiungi.Controls.Add(this.DataTimePicker_Data);
@@ -65,14 +76,14 @@
             this.tabPageAggiungi.Location = new System.Drawing.Point(4, 25);
             this.tabPageAggiungi.Name = "tabPageAggiungi";
             this.tabPageAggiungi.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageAggiungi.Size = new System.Drawing.Size(1172, 436);
+            this.tabPageAggiungi.Size = new System.Drawing.Size(1792, 871);
             this.tabPageAggiungi.TabIndex = 0;
             this.tabPageAggiungi.Text = "Aggiungi Spesa";
             this.tabPageAggiungi.UseVisualStyleBackColor = true;
             // 
             // textBox_Prezzo
             // 
-            this.textBox_Prezzo.Location = new System.Drawing.Point(20, 83);
+            this.textBox_Prezzo.Location = new System.Drawing.Point(505, 182);
             this.textBox_Prezzo.Name = "textBox_Prezzo";
             this.textBox_Prezzo.Size = new System.Drawing.Size(250, 22);
             this.textBox_Prezzo.TabIndex = 5;
@@ -80,21 +91,21 @@
             // ComboBox_Categoria
             // 
             this.ComboBox_Categoria.FormattingEnabled = true;
-            this.ComboBox_Categoria.Location = new System.Drawing.Point(20, 40);
+            this.ComboBox_Categoria.Location = new System.Drawing.Point(505, 139);
             this.ComboBox_Categoria.Name = "ComboBox_Categoria";
             this.ComboBox_Categoria.Size = new System.Drawing.Size(250, 24);
             this.ComboBox_Categoria.TabIndex = 1;
             // 
             // DataTimePicker_Data
             // 
-            this.DataTimePicker_Data.Location = new System.Drawing.Point(20, 123);
+            this.DataTimePicker_Data.Location = new System.Drawing.Point(505, 222);
             this.DataTimePicker_Data.Name = "DataTimePicker_Data";
             this.DataTimePicker_Data.Size = new System.Drawing.Size(250, 22);
             this.DataTimePicker_Data.TabIndex = 3;
             // 
             // button_Aggiungi
             // 
-            this.button_Aggiungi.Location = new System.Drawing.Point(20, 167);
+            this.button_Aggiungi.Location = new System.Drawing.Point(505, 266);
             this.button_Aggiungi.Name = "button_Aggiungi";
             this.button_Aggiungi.Size = new System.Drawing.Size(250, 30);
             this.button_Aggiungi.TabIndex = 4;
@@ -104,19 +115,21 @@
             // 
             // tabPageVisualizza
             // 
+            this.tabPageVisualizza.Controls.Add(this.label_Visualizza);
             this.tabPageVisualizza.Controls.Add(this.Visualizza_Spese);
             this.tabPageVisualizza.Location = new System.Drawing.Point(4, 25);
             this.tabPageVisualizza.Name = "tabPageVisualizza";
             this.tabPageVisualizza.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageVisualizza.Size = new System.Drawing.Size(1172, 436);
+            this.tabPageVisualizza.Size = new System.Drawing.Size(1792, 871);
             this.tabPageVisualizza.TabIndex = 1;
             this.tabPageVisualizza.Text = "Visualizza Spese";
             this.tabPageVisualizza.UseVisualStyleBackColor = true;
             // 
             // Visualizza_Spese
             // 
+            this.Visualizza_Spese.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Visualizza_Spese.HideSelection = false;
-            this.Visualizza_Spese.Location = new System.Drawing.Point(42, 37);
+            this.Visualizza_Spese.Location = new System.Drawing.Point(420, 191);
             this.Visualizza_Spese.Name = "Visualizza_Spese";
             this.Visualizza_Spese.Size = new System.Drawing.Size(1098, 373);
             this.Visualizza_Spese.TabIndex = 1;
@@ -124,23 +137,101 @@
             // 
             // tabPageReport
             // 
+            this.tabPageReport.Controls.Add(this.comboBox_Mesi);
             this.tabPageReport.Location = new System.Drawing.Point(4, 25);
             this.tabPageReport.Name = "tabPageReport";
             this.tabPageReport.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageReport.Size = new System.Drawing.Size(1172, 436);
+            this.tabPageReport.Size = new System.Drawing.Size(1792, 871);
             this.tabPageReport.TabIndex = 2;
             this.tabPageReport.Text = "Report Mensile";
             this.tabPageReport.UseVisualStyleBackColor = true;
             // 
+            // comboBox_Mesi
+            // 
+            this.comboBox_Mesi.FormattingEnabled = true;
+            this.comboBox_Mesi.Location = new System.Drawing.Point(25, 28);
+            this.comboBox_Mesi.Name = "comboBox_Mesi";
+            this.comboBox_Mesi.Size = new System.Drawing.Size(250, 24);
+            this.comboBox_Mesi.TabIndex = 2;
+            // 
             // tabPageImpostazioni
             // 
+            this.tabPageImpostazioni.Controls.Add(this.label_ImpostaBudget);
+            this.tabPageImpostazioni.Controls.Add(this.label_CambiaTema);
+            this.tabPageImpostazioni.Controls.Add(this.comboBox_CambiaTema);
+            this.tabPageImpostazioni.Controls.Add(this.textBox_Budget);
+            this.tabPageImpostazioni.Controls.Add(this.button_ImpostaBudget);
             this.tabPageImpostazioni.Location = new System.Drawing.Point(4, 25);
             this.tabPageImpostazioni.Name = "tabPageImpostazioni";
             this.tabPageImpostazioni.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageImpostazioni.Size = new System.Drawing.Size(1172, 436);
+            this.tabPageImpostazioni.Size = new System.Drawing.Size(1792, 871);
             this.tabPageImpostazioni.TabIndex = 3;
             this.tabPageImpostazioni.Text = "Impostazioni";
             this.tabPageImpostazioni.UseVisualStyleBackColor = true;
+            // 
+            // comboBox_CambiaTema
+            // 
+            this.comboBox_CambiaTema.FormattingEnabled = true;
+            this.comboBox_CambiaTema.Location = new System.Drawing.Point(631, 69);
+            this.comboBox_CambiaTema.Name = "comboBox_CambiaTema";
+            this.comboBox_CambiaTema.Size = new System.Drawing.Size(121, 24);
+            this.comboBox_CambiaTema.TabIndex = 2;
+            this.comboBox_CambiaTema.SelectedIndexChanged += new System.EventHandler(this.comboBox_CambiaTema_SelectedIndexChanged);
+            // 
+            // textBox_Budget
+            // 
+            this.textBox_Budget.Location = new System.Drawing.Point(289, 71);
+            this.textBox_Budget.Name = "textBox_Budget";
+            this.textBox_Budget.Size = new System.Drawing.Size(100, 22);
+            this.textBox_Budget.TabIndex = 1;
+            // 
+            // button_ImpostaBudget
+            // 
+            this.button_ImpostaBudget.Location = new System.Drawing.Point(289, 120);
+            this.button_ImpostaBudget.Name = "button_ImpostaBudget";
+            this.button_ImpostaBudget.Size = new System.Drawing.Size(100, 54);
+            this.button_ImpostaBudget.TabIndex = 0;
+            this.button_ImpostaBudget.Text = "Imposta Budget";
+            this.button_ImpostaBudget.UseVisualStyleBackColor = true;
+            this.button_ImpostaBudget.Click += new System.EventHandler(this.button_ImpostaBudget_Click);
+            // 
+            // label_CambiaTema
+            // 
+            this.label_CambiaTema.AutoSize = true;
+            this.label_CambiaTema.Location = new System.Drawing.Point(641, 29);
+            this.label_CambiaTema.Name = "label_CambiaTema";
+            this.label_CambiaTema.Size = new System.Drawing.Size(97, 17);
+            this.label_CambiaTema.TabIndex = 3;
+            this.label_CambiaTema.Text = "Imposta Tema";
+            // 
+            // label_ImpostaBudget
+            // 
+            this.label_ImpostaBudget.AutoSize = true;
+            this.label_ImpostaBudget.Location = new System.Drawing.Point(292, 29);
+            this.label_ImpostaBudget.Name = "label_ImpostaBudget";
+            this.label_ImpostaBudget.Size = new System.Drawing.Size(106, 17);
+            this.label_ImpostaBudget.TabIndex = 4;
+            this.label_ImpostaBudget.Text = "Imposta Budget";
+            // 
+            // label_Visualizza
+            // 
+            this.label_Visualizza.AutoSize = true;
+            this.label_Visualizza.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Visualizza.Location = new System.Drawing.Point(515, 128);
+            this.label_Visualizza.Name = "label_Visualizza";
+            this.label_Visualizza.Size = new System.Drawing.Size(883, 39);
+            this.label_Visualizza.TabIndex = 5;
+            this.label_Visualizza.Text = "VISUALIZZA TUTTE LE SPESE CHE HAI INSERITO:";
+            // 
+            // label_Aggiungi
+            // 
+            this.label_Aggiungi.AutoSize = true;
+            this.label_Aggiungi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Aggiungi.Location = new System.Drawing.Point(512, 86);
+            this.label_Aggiungi.Name = "label_Aggiungi";
+            this.label_Aggiungi.Size = new System.Drawing.Size(230, 24);
+            this.label_Aggiungi.TabIndex = 6;
+            this.label_Aggiungi.Text = "AGGIUNGI UNA SPESA";
             // 
             // DashBoard
             // 
@@ -156,6 +247,10 @@
             this.tabPageAggiungi.ResumeLayout(false);
             this.tabPageAggiungi.PerformLayout();
             this.tabPageVisualizza.ResumeLayout(false);
+            this.tabPageVisualizza.PerformLayout();
+            this.tabPageReport.ResumeLayout(false);
+            this.tabPageImpostazioni.ResumeLayout(false);
+            this.tabPageImpostazioni.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -171,5 +266,13 @@
         private System.Windows.Forms.Button button_Aggiungi;
         private System.Windows.Forms.TextBox textBox_Prezzo;
         private System.Windows.Forms.ListView Visualizza_Spese;
+        private System.Windows.Forms.ComboBox comboBox_Mesi;
+        private System.Windows.Forms.Button button_ImpostaBudget;
+        private System.Windows.Forms.TextBox textBox_Budget;
+        private System.Windows.Forms.ComboBox comboBox_CambiaTema;
+        private System.Windows.Forms.Label label_ImpostaBudget;
+        private System.Windows.Forms.Label label_CambiaTema;
+        private System.Windows.Forms.Label label_Visualizza;
+        private System.Windows.Forms.Label label_Aggiungi;
     }
 }
--- Progetto Dell'Anno 2024-25/Progetto Dell'Anno 2024-25/DashBoard.cs
+++ Progetto Dell'Anno 2024-25/Progetto Dell'Anno 2024-25/DashBoard.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Linq;
+using System.Drawing; // Per il Color
 
 namespace Progetto_Dell_Anno_2024_25
 {
     public partial class DashBoard : Form
     {
         private List<Spesa> listaSpese = new List<Spesa>();
+        private decimal budgetMensile;
 
         public DashBoard()
         {
@@ -18,18 +21,25 @@ namespace Progetto_Dell_Anno_2024_25
 
         private void button_AggiungiSpesa_Click(object sender, EventArgs e)
         {
-            if (ComboBox_Categoria.SelectedItem == null)
+            if (ComboBox_Categoria.SelectedItem == null || textBox_Prezzo.Text == "")
             {
                 MessageBox.Show("Compila tutti i campi!", "ERRORE", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
-            // Creazione di una nuova spesa
+
+            if (!textBox_Prezzo.Text.All(c => char.IsDigit(c) || c == ',' || c == '.'))
+            {
+                MessageBox.Show("Inserisci solo numeri validi nel campo prezzo.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             Spesa nuovaSpesa = new Spesa
             {
                 Categoria = ComboBox_Categoria.SelectedItem.ToString(),
-                Importo = Convert.ToDecimal(textBox_Prezzo.Text),
+                Importo = Convert.ToDecimal(textBox_Prezzo.Text.Replace(".", ",")),
                 Data = DataTimePicker_Data.Value
             };
+
             listaSpese.Add(nuovaSpesa);
             AggiornaTabella();
             PulisciCampi();
@@ -56,20 +66,80 @@ namespace Progetto_Dell_Anno_2024_25
 
         private void InizializzaCategorie()
         {
-            ComboBox_Categoria.Items.AddRange(new string[] { "Uscite", "Trasporti", "Bollette", "Visite", "Farmacia", "Mutuo", "Bolli", "Assicurazioni", "Altro" });
+            ComboBox_Categoria.Items.AddRange(new string[] { "Uscite", "Trasporti", "Bollette", "Visite", "Farmacia", "Mutuo", "Bolli", "Assicurazioni", "Varie" });
+            comboBox_Mesi.Items.AddRange(new string[] { "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre" });
+            comboBox_CambiaTema.Items.AddRange(new string[] { "Chiaro", "Scuro" });
         }
 
         private void InizializzaListView()
         {
             Visualizza_Spese.View = View.Details;
-            Visualizza_Spese.Columns.Add("Categoria", 200);
+            Visualizza_Spese.Columns.Add("Categoria", 100);
             Visualizza_Spese.Columns.Add("Importo", 100);
-            Visualizza_Spese.Columns.Add("Data", 150);
+            Visualizza_Spese.Columns.Add("Data", 100);
         }
 
         private void ImpostaControlli()
         {
             tabControlOperazioni.SelectedIndex = 0;
+        }
+
+        private void button_ImpostaBudget_Click(object sender, EventArgs e)
+        {
+            //controllo se la casella è vuota e se ci sono solo numeri
+            if (textBox_Budget.Text != "" && textBox_Budget.Text.All(char.IsDigit))
+            {
+                budgetMensile = Convert.ToDecimal(textBox_Budget.Text);
+                MessageBox.Show($"Budget impostato: {budgetMensile:C}");
+                textBox_Budget.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Inserisci solo numeri interi nel budget!", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                textBox_Budget.Clear();
+            }
+        }
+
+        private void comboBox_CambiaTema_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox_CambiaTema.SelectedItem != null)
+                CambiaTema(comboBox_CambiaTema.SelectedItem.ToString());
+        }
+
+        private void CambiaTema(string tema)
+        {
+            if (tema == "Scuro")
+            {
+                this.BackColor = Color.Black;
+                foreach (Control c in this.Controls)
+                {
+                    c.ForeColor = Color.Black;
+                    tabPageAggiungi.BackColor = Color.Black;
+                    tabPageVisualizza.BackColor = Color.Black;
+                    tabPageReport.BackColor = Color.Black;
+                    tabPageImpostazioni.BackColor = Color.Black;
+                    label_ImpostaBudget.ForeColor = Color.White;
+                    label_CambiaTema.ForeColor = Color.White;
+                    label_Visualizza.ForeColor = Color.White;
+                    label_Aggiungi.ForeColor = Color.White;
+                }   
+            }
+            else
+            {
+                this.BackColor = SystemColors.Control;
+                foreach (Control c in this.Controls)
+                {
+                    c.ForeColor = Color.Black;
+                    tabPageAggiungi.BackColor = Color.White;
+                    tabPageVisualizza.BackColor = Color.White;
+                    tabPageReport.BackColor = Color.White;
+                    tabPageImpostazioni.BackColor = Color.White;
+                    label_ImpostaBudget.ForeColor = Color.Black;
+                    label_CambiaTema.ForeColor = Color.Black;
+                    label_Visualizza.ForeColor = Color.Black;
+                    label_Aggiungi.ForeColor = Color.Black;
+                }
+            }
         }
     }
 
